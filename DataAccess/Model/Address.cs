fcd9b59bb86492e598f5d3de6dd27c2ac711db4a
Tomas Chmelik <chmelik.tomas@email.cz>
2021-11-11 10:32:54 +0100

    DataAccess project, person class

---
+++ DataAccess/Model/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model
+{
+    class Address
+    {
+
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public int Zipcode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public Address(string street, string houseNumber, int zipcode, string city, string country)
+        {
+            Street = street;
+            HouseNumber = houseNumber;
+            Zipcode = zipcode;
+            City = city;
+            Country = country;
+        }
+    }
+}
---
+++ DataAccess/Model/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model
+{
+    class Artist : Person
+    {
+        public string ProfileDescription { get; set; }
+    }
+}
