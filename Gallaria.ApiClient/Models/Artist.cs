6cea0e6cbe2c6e6217bbf5965cba71a74a6bb64f
NXTsk <mattejhorvath@gmail.com>
2021-11-18 13:46:31 +0100

    Website create account UI added

---
+++ Gallaria.ApiClient/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallaria.ApiClient.Models
+{
+    class Artist : Person
+    {
+        public string ProfileDescription { get; set; }
+
+    }
+}
