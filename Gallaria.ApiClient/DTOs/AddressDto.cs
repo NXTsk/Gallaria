eb1f6ba7b12dd91959269c37305cb86ba8d091c4
NXTsk <mattejhorvath@gmail.com>
2021-11-21 23:15:57 +0100

    confirmation of password works

--- Gallaria.ApiClient/DTOs/AddressDto.cs
+++ Gallaria.ApiClient/DTOs/AddressDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,19 @@ namespace Gallaria.ApiClient.DTOs
 {
     public class AddressDto
     {
+        [Required(ErrorMessage = "Street is required.")]
         public string Street { get; set; }
+
+        [Required(ErrorMessage = "House number is required.")]
         public string HouseNumber { get; set; }
+
+        [Required(ErrorMessage = "Zipcode is required.")]
         public int Zipcode { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Country is required.")]
         public string Country { get; set; }
     }
 }
