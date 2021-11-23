c08886add505440fe84f7ce7453f80618916f5a2
denisa122 <neagudenisa12@yahoo.com>
2021-11-23 13:28:54 +0100

    art details

---
+++ Gallaria.WEB/Controllers/ArtController.cs
@@ -0,0 +1,24 @@
+﻿using Gallaria.ApiClient.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gallaria.ApiClient;
+
+namespace Gallaria.WEB.Controllers
+{
+    public class ArtController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ArtDetails(int id)
+        {
+            ArtDto art = ApiClient.ArtController.GetArtByIDAsync(id).Result;
+            return View(art);
+        }
+    }
+}
