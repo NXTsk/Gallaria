a2bc8d72662ce30a07669f6feb48038e74d403a6
Tomas Chmelik <chmelik.tomas@email.cz>
2021-11-29 09:29:11 +0100

    From allArts to art details

--- Gallaria.API/Model/ArtDto.cs
+++ Gallaria.API/Model/ArtDto.cs
@@ -7,6 +7,7 @@ namespace Gallaria.API.Model
 {
     public class ArtDto
     {
+        public int Id { get; set; }
         public int AuthorId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
--- Gallaria.ApiClient/DTOs/ArtDto.cs
+++ Gallaria.ApiClient/DTOs/ArtDto.cs
@@ -9,12 +9,11 @@ namespace Gallaria.ApiClient.DTOs
 {
     public class ArtDto
     {
+        public int Id { get; set; }
         public int AuthorId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-
         public string Image { get; set; }
-
         public decimal Price { get; set; }
         public int AvailableQuantity { get; set; }
         public string Category { get; set; }
